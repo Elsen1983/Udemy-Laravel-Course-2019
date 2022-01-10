b9671b99bb5b779af2fd8e93a162e4a91e267aff
Elsen1983 <norbertkorom@gmail.com>
2022-01-10 14:28:59 +0000

    separating the layouts and using more @extends and @yield in them

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel Tutorial') }}</title>
+    <title>
+        @yield('title')
+    </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -27,7 +29,23 @@
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
-
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+        <!-- Navbar content -->
+        <div class="container-fluid">
+            <a href="/" class="navbar-brand">ToDo App</a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="/" class="nav-item nav-link active">Home</a>
+                    <a href="/todo" class="nav-item nav-link">ToDo</a>
+                    <a href="/about" class="nav-item nav-link">About</a>
+                    <a href="/contact" class="nav-item nav-link">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
     <main class="py-4">
         @yield('content')
     </main>
--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+
+@section('title')
+   Todo:  {{ $todo->name }}
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
